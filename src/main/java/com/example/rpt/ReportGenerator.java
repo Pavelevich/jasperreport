00d231923cb0c43dec7e06a6e596c22f6b1532c0
develChmirenko <developer31f@gmail.com>
2024-06-17 08:47:50 +0200

    Documentation plus.

--- src/main/java/com/example/rpt/ReportGenerator.java
+++ src/main/java/com/example/rpt/ReportGenerator.java
@@ -15,63 +15,15 @@ import net.sf.jasperreports.engine.xml.JRXmlLoader;
 import net.sf.jasperreports.export.SimpleExporterInput;
 import net.sf.jasperreports.export.SimpleOutputStreamExporterOutput;
 
-//public class ReportGenerator {
-//    public static void main(String[] args) {
-//        try {
-//            File reportFile = new File("C:\\Users\\devel\\IdeaProjects\\rpt\\src\\main\\resources\\report\\test.jrxml");
-//
-//            JasperDesign jasperDesign = JRXmlLoader.load(reportFile);
-//            JasperReport jasperReport = JasperCompileManager.compileReport(jasperDesign);
-//
-//            JasperPrint jasperPrint = JasperFillManager.fillReport(jasperReport, new HashMap<>(), new JREmptyDataSource());
-//
-//            String outputPath = "C:\\output\\report.pdf";
-//
-//            JasperExportManager.exportReportToPdfFile(jasperPrint, outputPath);
-//
-//            System.out.println("PDF report has been generated and saved at: " + outputPath);
-//
-//        } catch (JRException e) {
-//            System.out.println("Error generating report: " + e.getMessage());
-//        }
-//    }
-//}
-//public class ReportGenerator {
-//    public static void main(String[] args) {
-//        Connection conn = null;
-//        try {
-//
-//            String jdbcDriver = "com.microsoft.sqlserver.jdbc.SQLServerDriver";
-//            String jdbcUrl= "jdbc:sqlserver://localhost:1433;databaseName=master;user=sa;password=<YourPassword>;encrypt=true;trustServerCertificate=true";
-//
-//            Class.forName(jdbcDriver);
-//            conn = DriverManager.getConnection(jdbcUrl);
-//
-//            File reportFile = new File("C:\\Users\\devel\\IdeaProjects\\rpt\\src\\main\\resources\\report\\test.jrxml");
-//
-//            JasperDesign jasperDesign = JRXmlLoader.load(reportFile);
-//            JasperReport jasperReport = JasperCompileManager.compileReport(jasperDesign);
-//
-//            JasperPrint jasperPrint = JasperFillManager.fillReport(jasperReport, new HashMap<>(), conn);
-//
-//            String outputPath = "C:\\output\\report.pdf";
-//
-//            JasperExportManager.exportReportToPdfFile(jasperPrint, outputPath);
-//
-//            System.out.println("PDF report has been generated and saved at: " + outputPath);
-//
-//        } catch (Exception e) {
-//            System.out.println("Error generating report: " + e.getMessage());
-//        } finally {
-//            try {
-//                if (conn != null && !conn.isClosed()) {
-//                    conn.close();
-//                }
-//            } catch (SQLException ignored) {}
-//        }
-//    }
-//}
+
+/**
+ * The ReportGenerator class is responsible for generating a report in different formats based on a given JasperReport template.
+ * It connects to a database, retrieves data, fills the report template, and exports it to a specified output path.
+ */
 public class ReportGenerator {
+    /**
+     *
+     */
     public static void main(String[] args) {
         Connection conn = null;
         try {
@@ -86,6 +38,15 @@ public class ReportGenerator {
 
             JasperDesign jasperDesign = JRXmlLoader.load(reportFile);
             JasperReport jasperReport = JasperCompileManager.compileReport(jasperDesign);
+
+//            Passing parameters to be used in the pdf. If you want to use large amounts of data, using DB query is more optimal.
+//            Map<String, Object> parameters = new HashMap<>();
+//            // Adding param on map
+//            parameters.put("Param1", "Val1");
+//            parameters.put("Param", "Val2");
+//            ...
+//            //
+//            JasperPrint jasperPrint = JasperFillManager.fillReport(jasperReport, parameters, new JREmptyDataSource());
 
             JasperPrint jasperPrint = JasperFillManager.fillReport(jasperReport, new HashMap<>(), conn);
 
